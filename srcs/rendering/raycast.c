4971937e6f7b51910fc88c1140cbbf0e75aeb63a
luis <ldias-da@student.42porto.com>
2026-06-04 18:44:49 +0100

    Fixed textures

--- srcs/rendering/raycast.c
+++ srcs/rendering/raycast.c
@@ -141,38 +141,31 @@ int	get_pixel_from_texture(t_img *img, int tex_x, int tex_y)
 	return (*(int *)pixel);
 }
 
-void	draw_vertical_texture(t_core *core, int x, int draw_start, int draw_end)
+t_img	*get_texture(t_core *core)
 {
-	double	wall_x;
-	int	tex_x;
-	int	tex_y;
-	int	y;
 	t_img	*tex;
-	
-	if (x < 0 || x >= WIDTH)
-		return;
-	// if (draw_start < 0)
-	// 	draw_start = 0;
-	// if (draw_end >= HEIGHT)
-	// 	draw_end = HEIGHT - 1;
-	y = draw_start;
+
 	if (core->ray.side == 0)
 	{
-		wall_x = core->player.y + core->ray.perp_wall_dist * core->ray.ray_dir_y;
-		if (core->ray.ray_dir_y > 0)
-			tex = &core->textures.no_img;
-		else
-			tex = &core->textures.so_img;
-	}
-	else
-	{
-		wall_x = core->player.x + core->ray.perp_wall_dist * core->ray.ray_dir_x;
 		if (core->ray.ray_dir_x > 0)
 			tex = &core->textures.we_img;
 		else
 			tex = &core->textures.ea_img;
 	}
-	wall_x -= floor(wall_x);
+	else
+	{
+		if (core->ray.ray_dir_y > 0)
+			tex = &core->textures.no_img;
+		else
+			tex = &core->textures.so_img;
+	}
+	return (tex);
+}
+
+int	get_tex_x(t_core *core, double wall_x, t_img *tex)
+{
+	int	tex_x;
+
 	tex_x = (int)(wall_x * (double)tex->width);
 	if (tex_x < 0)
 		tex_x = 0;
@@ -182,15 +175,34 @@ void	draw_vertical_texture(t_core *core, int x, int draw_start, int draw_end)
 		tex_x = tex->width - tex_x - 1;
 	if(core->ray.side == 1 && core->ray.ray_dir_y > 0)
 		tex_x = tex->width - tex_x - 1;
+	return (tex_x);
+}
+
+void	draw_vertical_texture(t_core *core, int x, int draw_start, int draw_end)
+{
+	double	wall_x;
+	int		tex_x;
+	int		tex_y;
+	int		y;
+	t_img	*tex;
+	
+	if (x < 0 || x >= WIDTH)
+		return;
+	if (core->ray.side == 0)
+		wall_x = core->player.y + core->ray.perp_wall_dist * core->ray.ray_dir_y;
+	else
+		wall_x = core->player.x + core->ray.perp_wall_dist * core->ray.ray_dir_x;
+	wall_x -= floor(wall_x);
+	tex = get_texture(core);
+	tex_x = get_tex_x(core, wall_x, tex);
 	double	step = (double)tex->height / (double)(draw_end - draw_start + 1);
 	float	tex_pos = 0;
 	tex_y = 0;
+	y = draw_start;
 	while (y <= draw_end)
 	{
 		tex_y = (int)tex_pos;
-		put_pixel(core, x, y, get_pixel_from_texture(tex,
-					tex_x,
-					tex_y));
+		put_pixel(core, x, y, get_pixel_from_texture(tex, tex_x, tex_y));
 		tex_pos += step;
 		y++;
 	}
